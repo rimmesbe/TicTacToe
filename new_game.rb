075f8a638e696a571e2a890b92ec2ebffb6a5bb8
rimmesbe <ryanimmesberger@gmail.com>
2015-11-07 21:45:38 -0500

    created Player class with initialization method

--- new_game.rb
+++ new_game.rb
@@ -1,1 +1,9 @@
-new_game.rb
+class Player
+  attr_reader :player_name, :player_type, :player_symbol
+
+  def initialize(args)
+    @player_name = args[:player_name]
+    @player_type = args[:player_type]
+    @player_symbol = args[:player_symbol]
+  end
+end
