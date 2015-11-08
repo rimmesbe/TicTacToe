7265178735bfd9688c76267195eaf020e2bf5c17
rimmesbe <ryanimmesberger@gmail.com>
2015-11-07 23:58:42 -0500

    spelling fixes

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,13 +11,13 @@ describe Board do
     end
   end
 
-  describe "#{}current_board" do
+  describe "#current_board" do
     it "returns current board" do
       expect(@new_board.current_board).to eq ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     end
   end
 
-  describe "#{}update_board" do
+  describe "#update_board" do
     it "changes one spot on current board" do
       @new_board.update_board("4", "X")
       expect(@new_board.current_board).to eq ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
