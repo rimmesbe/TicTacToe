d7e5deff8599a072a61fc1afdbd066b718d092de
rimmesbe <ryanimmesberger@gmail.com>
2015-11-09 19:29:56 -0500

    implemented get_best_move method for computer players

--- new_game.rb
+++ new_game.rb
@@ -22,16 +22,53 @@ class Game
     current_player = @player_one.symbol == "X" ? @player_one : @player_two
     until game_over(@board.current_board) || tie(@board.current_board)
       puts "#{current_player.name} make your move..."
-      current_move = current_player.type == "human" ? current_player.get_move : current_player.get_move(get_best_move)
+      current_move = current_player.type == "human" ? current_player.get_move : current_player.get_move(get_best_move(@board.current_board, current_player))
       @board.update(current_move, current_player.symbol)
-      current_player = @player_one.symbol == current_player.symbol ? @player_two : @player_one
+      current_player = player_swap(current_player)
       puts @board
     end
     puts "Game Over"
   end
 
-  def get_best_move
-    4
+  def player_swap(current_player)
+    @player_one == current_player ? @player_two : @player_one
+  end
+
+  def get_best_move(board, current_player)
+    available_spaces = []
+    best_move = nil
+    player_symbol = current_player.symbol
+    opponent_symbol = player_swap(current_player).symbol
+    p "opponents symbol is #{opponent_symbol}"
+
+    board.each do |s|
+      if s != "X" && s != "O"
+        available_spaces << s
+      end
+    end
+    available_spaces.each do |as|
+      board[as.to_i] = player_symbol
+      if game_over(board)
+        best_move = as.to_i
+        board[as.to_i] = as
+        return best_move
+      else
+        board[as.to_i] = opponent_symbol
+        if game_over(board)
+          best_move = as.to_i
+          board[as.to_i] = as
+          return best_move
+        else
+          board[as.to_i] = as
+        end
+      end
+    end
+    if best_move
+      return best_move
+    else
+      n = rand(0..available_spaces.count)
+      return available_spaces[n].to_i
+    end
   end
 
   def game_over(b)
