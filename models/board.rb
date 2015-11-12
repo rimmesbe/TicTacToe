e9cac83cbbb010313fa929375d00361d77fc5249
rimmesbe <ryanimmesberger@gmail.com>
2015-11-11 21:01:15 -0500

    created Tic_Tac_Toe_Board subclass off of Board class

--- models/board.rb
+++ models/board.rb
@@ -1,10 +1,9 @@
 class Board
   attr_reader :current_board, :rows, :columns
-  def initialize(rows, columns)
-    @current_board = []
-    (rows * columns).times {|idx| @current_board << idx.to_s}
+  def initialize(rows = 0, columns = 0)
     @rows = rows
     @columns = columns
+    @current_board = set_board_up
   end
 
   def update(index, value)
@@ -28,5 +27,25 @@ class Board
   def spot_format(spot)
     spot.to_s.length == 1 ? " "+spot.to_s+" " : " "+spot.to_s
   end
+
+  def set_board_up
+    spot_array = []
+    (rows * columns).times {|idx| spot_array << idx.to_s}
+    spot_array
+  end
 end
 
+class Tic_Tac_Toe_Board < Board
+  attr_reader :current_board
+
+  def initialize
+    @columns = 3
+    @rows = 3
+    @current_board = set_board_up
+  end
+
+
+end
+
+# t = Tic_Tac_Toe_Board.new
+# puts t
