792e060b4760cb3f2d55af3df2f0be1f0ae5c8f1
rimmesbe <ryanimmesberger@gmail.com>
2015-11-19 14:16:55 -0500

    In player spec: took out instance variables and swapped in let statements

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,64 +3,65 @@ require_relative '../models/player'
 describe Player do
 
   before :each do
-      @player_human = Player.new(name: "Jacob", type: "human", symbol: "X")
-      @player_computer = Player.new(name: "comp1", type: "computer", symbol: "O")
       allow($stdout).to receive(:puts)
   end
 
+  let(:player_human) {Player.new(name: "Jacob", type: "human", symbol: "X")}
+  let(:player_computer) {Player.new(name: "comp1", type: "computer", symbol: "O")}
+
   describe "#new" do
     it "takes 3 parameters and returns a Player object" do
-      expect(@player_human).to be_a Player
+      expect(player_human).to be_a Player
     end
   end
 
   describe "#name" do
     it "returns the correct name" do
-      expect(@player_human.name).to eq "Jacob"
+      expect(player_human.name).to eq "Jacob"
     end
   end
 
   describe "#type" do
     it "returns the correct type" do
-      expect(@player_human.type).to eq "human"
+      expect(player_human.type).to eq "human"
     end
   end
 
   describe "#symbol" do
     it "returns the correct symbol" do
-      expect(@player_human.symbol).to eq "X"
+      expect(player_human.symbol).to eq "X"
     end
   end
 
   describe "#get_move" do
     it "returns the correct players move" do
-      allow(@player_human).to receive(:gets).and_return("8\n")
-      expect(@player_human.get_move).to eq "8"
+      allow(player_human).to receive(:gets).and_return("8\n")
+      expect(player_human.get_move).to eq "8"
     end
 
     it "returns suggested move if type != human" do
-      expect(@player_computer.get_move("4")).to eq "4"
+      expect(player_computer.get_move("4")).to eq "4"
     end
   end
 
   describe "#create_name" do
     it "returns user input as new name of player" do
-      allow(@player_human).to receive(:gets).and_return("Joe\n")
-      expect(@player_human.create_name).to eq "Joe"
+      allow(player_human).to receive(:gets).and_return("Joe\n")
+      expect(player_human.create_name).to eq "Joe"
     end
   end
 
   describe "#create_symbol" do
     it "returns user input as new symbol of player" do
-      allow(@player_human).to receive(:gets).and_return("O\n")
-      expect(@player_human.create_symbol).to eq "O"
+      allow(player_human).to receive(:gets).and_return("O\n")
+      expect(player_human.create_symbol).to eq "O"
     end
   end
 
   describe "#choose_type" do
     it "returns user input as new type of player" do
-      allow(@player_human).to receive(:gets).and_return("computer\n")
-      expect(@player_human.choose_type).to eq "computer"
+      allow(player_human).to receive(:gets).and_return("computer\n")
+      expect(player_human.choose_type).to eq "computer"
     end
   end
 
